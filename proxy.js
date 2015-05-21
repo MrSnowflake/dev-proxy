5bc8a3b5f975fb6af3539ff87ced570e5dc814fa
Maarten Krijn <maarten.krijn@amplexor.com>
2015-05-21 09:41:18 +0200

    Added support for a config object

--- proxy.js
+++ proxy.js
@@ -5,14 +5,14 @@ var proxy = new hoxy.Proxy().listen(8080);
 
 var filePath = '/Users/maarten.krijn/amplexor/Projects/GEA/';
 
-console.log('starting');
+var config = [
+	{ path: 'http://preview-gea.dev.amplexor.com/js/*', localPath: '/Users/maarten.krijn/amplexor/Projects/GEA/'},
+	{ path: 'http://preview-gea.dev.amplexor.com/css/*', localPath: '/Users/maarten.krijn/amplexor/Projects/GEA/'}
+];
 
-proxy.intercept({
-		phase: 'response',
-		fullUrl: 'http://preview-gea.dev.amplexor.com/js/*',
-		as: 'string'
-	}, function(req, response, done) {
-		var file = path.join(filePath, req.url);
+function action(item) {
+	return function(req, response, done) {
+		var file = path.join(item.localPath, req.url);
 		console.log('send', file);
 		fs.readFile(file, {encoding: 'utf-8'}, function(err,data){
 			if (!err) {
@@ -23,4 +23,20 @@ proxy.intercept({
 			done();
 		});
 	}
-);
+}
+
+function route(item) {
+	proxy.intercept({
+			phase: 'response',
+			fullUrl: item.path,
+			as: 'string'
+		}, action(item)
+	);
+}
+
+console.log('starting');
+for (var index = 0; index < config.length; index++) {
+	var configItem = config[index];
+	
+	route(configItem);
+}
