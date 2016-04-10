9e69ad282a87c0c24bf62ef95bb77361d6b1a2d9
Maarten Krijn <Maarten.Krijn@amplexor.com>
2016-04-10 21:51:34 +0200

    Added tests. Added fixes for tests.

---
+++ route-container.js
@@ -0,0 +1,80 @@
+"use strict"
+
+module.exports = () => {
+	const routeDomains = {};
+	const routesById = {};
+	let maxId = 0;
+	
+	const isValidRoute = route => {
+		return route.path && route.localPath && route.domain;
+	};
+
+	const getRoutesFor = route => {
+		let routes = {};
+		if (routeDomains.hasOwnProperty(route.domain)) {
+			routes = routeDomains[route.domain];
+		} else {
+			routeDomains[route.domain] = routes;
+		}
+
+		return routes;
+	}
+
+	const routeExists = route => {
+		let routes = getRoutesFor(route);
+		return routes.hasOwnProperty(route.path);
+	};
+
+	let self = {
+		add: route => {
+			if (!isValidRoute(route))
+				throw "Invalid route";
+			 else if (routeExists(route)) {
+				throw 'Route already exists';
+			}
+
+			let routes = getRoutesFor(route);
+
+			route.id = maxId;
+			routes[route.path] = route;
+			routesById[maxId] = route;
+
+			maxId++;
+		},
+		put: (routeId, route) => {
+			if (!isValidRoute(route))
+				throw "Invalid route";
+
+			let routes = getRoutesFor(route);
+
+			let oldRoute = routesById[routeId];
+
+			delete routes[oldRoute.path];
+
+			route.id = oldRoute.id;
+			routes[route.path] = route;
+			routesById[route.id] = route;
+		},
+		get: route => {
+			return routesById[route];
+		},
+		delete: routeId => {
+			if (!self.hasRoute(routeId))
+				throw 'Route not found';
+
+			let route = routesById[routeId];
+
+			var routes = routeDomains[route.domain];
+			delete routes[route.path];
+			if (route.length === 0)
+				delete routeDomains[route.domain];
+
+			delete routesById[routeId];
+		},
+		hasRoute: routeId => {
+			return !!routesById[routeId];
+		}
+	};
+
+	return self;
+};
