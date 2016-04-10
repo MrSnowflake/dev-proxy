fb2fa249b89765224826e0c69b417cba03525c8a
Maarten Krijn <Maarten.Krijn@amplexor.com>
2016-04-10 13:16:35 +0200

    Added POST, PUT, Delete operations. Now routes are creatable from REST

--- proxy.js
+++ proxy.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var fs = require('fs');
 var path = require('path');
 var hoxy = require('hoxy');
@@ -41,3 +43,117 @@ for (var index = 0; index < config.length; index++) {
 	
 	route(configItem);
 }
+
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var routes = {};
+var routesByPath = {};
+var maxId = 0;
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/routes', function (req, res) {
+	let routesArray = [];
+
+	for (var route in routes) {
+	    if (routes.hasOwnProperty(route)) {
+	        routesArray.push(routes[route]);
+	    }
+	}
+	
+	res.send(routesArray);
+});
+
+/*
+409 Conflict
+403 Forbidden
+*/
+app.post('/routes', function (req, res) {
+	let newRoute = req.body;
+	
+	res.type('json');
+	
+	if (!newRoute.path || !newRoute.localPath) {
+		let error = {};
+		error.message = "Invalid route. Provide all fields.";
+		res.status(403).send(error);
+		return;
+	} else if (routesByPath[newRoute.path]) {
+		let error = {};
+		error.message = "Route already exists. If updating use PUT.";
+		res.status(409).send(error);
+		return;
+	}
+
+	newRoute.id = maxId;
+	console.log(newRoute);
+	routes[maxId] = newRoute;
+	routesByPath[newRoute.path] = newRoute;
+
+	maxId++;
+	
+	route(newRoute);
+
+	res.status(201).send({message:'Created'});
+});
+
+app.put('/routes/:id', function (req, res) {
+	let newRoute = req.body;
+	
+	res.type('json');
+	
+	if (!newRoute.path || !newRoute.localPath) {
+		let error = {};
+		error.message = "Invalid route. Provide all fields.";
+		res.status(403).send(error);
+		return;
+	} else if (!routes[req.params.id]) {
+		let error = {};
+		error.message = "Route not found.";
+		res.status(404).send(error);
+		return;
+	}
+
+	let oldRoute = routes[req.params.id];
+
+	newRoute.id = parseInt(req.params.id);
+	console.log(newRoute);
+	routes[req.params.id] = newRoute;
+	delete routesByPath[oldRoute.path];
+	routesByPath[newRoute.path] = newRoute;
+
+	route(newRoute);
+
+	res.status(200).send({message:'Ok'});
+});
+
+app.delete('/routes/:id', function (req, res) {
+	let newRoute = req.body;
+	
+	res.type('json');
+	
+	if (!routes[req.params.id]) {
+		let error = {};
+		error.message = "Route not found.";
+		res.status(404).send(error);
+		return;
+	}
+
+
+
+	let oldRoute = routes[req.params.id];
+	delete routesByPath[oldRoute.path];
+	delete routes[req.params.id];
+
+	//delete route
+
+	res.status(200).send({message:'Ok'});
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
